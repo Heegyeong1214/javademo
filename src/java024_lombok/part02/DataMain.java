10a9b00a1d42ef024ca70ca542d32c20cca8c7de
Yu heegyeong <greenaryds@gamil.com>
2025-06-02 12:36:59 +0900

    20250602 | lombok

--- src/java024_lombok/part02/DataMain.java
+++ src/java024_lombok/part02/DataMain.java
@@ -1,7 +1,7 @@
 package java024_lombok.part02;
 
 import lombok.extern.slf4j.Slf4j;
-
+ 
 @Slf4j
 public class DataMain {
 
@@ -14,11 +14,13 @@ public class DataMain {
 		dto.setLoc("경기");
 		
 		System.out.printf("%d %s %d %s\n", dto.getNum(), dto.getName(), dto.getAge(), dto.getLoc());
-		//System.out.println(dto.toString());	
-		log.info("mem", dto);
+		//멤버 변수에 있는 값을 리턴해줌
+		System.out.println(dto.toString());
+		//객체에 대한 정보 리턴
+		log.info("mem", dto.toString());
 		
-		 MemDTO mem = new MemDTO(2,"여진구", 40, "서울");
-		 System.out.println(mem.toString());
+//		 MemDTO mem = new MemDTO(2,"여진구", 40, "서울");
+//		 System.out.println(mem.toString());
 		
 	}
 
--- src/java024_lombok/part02/MemDTO.java
+++ src/java024_lombok/part02/MemDTO.java
@@ -7,17 +7,26 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data
-//@AllArgsConstructor
-//@ToString
-//@Getter
-//@Setter
-//
-//@NoArgsConstructor
+//@Data
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+
+@NoArgsConstructor //인자 값이 없는 Method -> 	
 public class MemDTO {
 	private int num;
 	private String name;
 	private int age;
 	private String loc;
+	
+	//@NoArgsConstructor -> 인자 값이 없는 Method
+	// public MemDTO() {}
+	
+	// @AllArgsConstructor -> 모든 생성자가 있는 Method
+	//public MemDTO (int num, String name,int age, String loc) {
+	//	this.name = name;
+	//	this.num=num;
+	//}
 
 }
